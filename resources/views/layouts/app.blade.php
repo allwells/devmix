e8283eaae4b828b250acad6dd3ad437372529896
Allwell Onen <aleenfestus@gmail.com>
2022-04-23 08:44:19 +0100

    Adding URL::forceScheme('https') in production environments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posty</title>
+    <title>DevMix</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/trix.css') }}">
@@ -20,7 +20,7 @@
             {{-- logo --}}
             <a class="navbar-brand fw-bolder" href="@auth {{ route('dashboard') }} @endauth
                 @guest {{ route('posts') }} @endguest">
-                Posty
+                dev.Mix()
             </a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
